fd02e67e5a3280b29623d181e7a19848447191a8
antonimateus <127150996+antonimateus@users.noreply.github.com>
2023-06-13 09:56:16 -0300

    Add files via upload

---
+++ javas-main/antoni/meuprimeiroprograma.js
@@ -0,0 +1,11 @@
+console.log("meu primeiros programa:trabalhando com variaveis")
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadesomada = idade+2;
+console.log(idadesomada);
