1ec4c37265d6f4e19110639f52273b3074f3a51e
antonimateus <127150996+antonimateus@users.noreply.github.com>
2023-06-20 11:19:26 -0300

    Add files via upload

---
+++ antoni/4-conversaodearquivos.js
@@ -0,0 +1,11 @@
+console.log("conversao de tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseint("2") + parseint("2"));
+
+console.log("7" / "2");
+console.log("ricardo" / "2"); //not a number
+
+console.log(6.5);
+console.log(6.5);
