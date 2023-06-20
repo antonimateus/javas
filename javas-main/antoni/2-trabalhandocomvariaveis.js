71b6fdfc13398505c78baf570c72608a5b12eb6e
antonimateus <127150996+antonimateus@users.noreply.github.com>
2023-06-20 11:18:40 -0300

    Delete javas-main directory

--- javas-main/antoni/2-trabalhandocomvariaveis.js
+++
@@ -1,9 +0,0 @@
-//Console.log("trabalhando com variaveis");
-console.log("trabalhando com variaveis");
-//js e caasse sensitive
-
-idade = 29;
-const nome = "ricardo";
-
-//nao fazer
-//mes = "janeiro";
