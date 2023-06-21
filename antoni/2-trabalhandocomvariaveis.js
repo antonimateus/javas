8c3cf3025e6d5faaeae38399cc78efc4abfeabf8
antonimateus <127150996+antonimateus@users.noreply.github.com>
2023-06-21 07:48:57 -0300

    Add files via upload

---
+++ antoni/2-trabalhandocomvariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("trabalhando com variaveis");
+console.log("trabalhando com variaveis");
+//js e caasse sensitive
+
+idade = 29;
+const nome = "ricardo";
+
+//nao fazer
+//mes = "janeiro";
---
+++ antoni/5-atribuicaodevariaveis.js
@@ -0,0 +1,12 @@
+console.log("trabalhando com atribuicao de variaveis");
+idade = 29;
+let nome = "ricardo";
+const sobrenome = "bugan";
+
+
+//console.log(nome + " " + sobrenomeme)
+console.log(nome, sobrenomeme)
+console.log('meu nome e ${nome} ${sobrenome}');
+
+nome = nome + sobrenome;
+console.log
